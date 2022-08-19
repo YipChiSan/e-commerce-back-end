3e98fe74f2eaeeae03b9821369b72ed7d69ffaef
YipChiSan <zye39@yahoo.com>
2022-08-19 15:55:06 +1000

    Change to async functions

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,14 +4,17 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{model: Category}, {model: ProductTag, include: [{model: Tag}]}],
+      include: [{model: Category}, 
+                {model: ProductTag, 
+                  include: [{model: Tag}],
+                }],
     });
-
+    
     if (!productData) {
       res.status(404).json({ message: 'No product data found'});
       return;
@@ -23,7 +26,7 @@ router.get('/', (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
@@ -117,7 +120,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
     const productData = await Product.destroy({
